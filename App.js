8c94df2ce05d482cdff2ee0502dd0120eb422fb5
GuiMello83 <gui.vazdemello@gmail.com>
2022-07-10 02:08:00 -0300

    Final

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ export default function App() {
     const fistNumber = parseFloat(splitNumbers[0])
     const lastNumber = parseFloat(splitNumbers[2])
     const operator = splitNumbers[1]
+    
 
     // Faz ação referente tecla pressionada
     switch (operator) {
